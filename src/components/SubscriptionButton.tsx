d847468b311ea44d881ea766ef300c0daa840a9c
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-05-05 02:45:25 +0530

    Button changed

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -21,7 +21,7 @@ const SubscriptionButton = (props: Props) => {
     }
   };
   return (
-    <Button disabled={loading} onClick={handleSubscription} variant="outline">
+    <Button disabled={loading} onClick={handleSubscription}>
       {props.isPro ? "Manage Subscriptions" : "Get Pro"}
     </Button>
   );
